e3610baab8a1ec2515f501d4463fbbb7da5f330c
amitkanjariya <amitkanjariya28@gmail.com>
2024-10-29 01:59:49 +0530

    sidebar and navbar done

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
 const DashBoardLayout = ({
     children
 } : {
@@ -5,11 +8,12 @@ const DashBoardLayout = ({
 }) => {
     return (
         <div className="h-full relative">
-            <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-                Hello SideBar
+            <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900 text-white">
+                <Sidebar/>
             </div>
             <div>
-                <main className="md:pl-72">Hello Content 
+                <main className="md:pl-72">
+                    <Navbar/>
                     {children}
                 </main>
             </div>
